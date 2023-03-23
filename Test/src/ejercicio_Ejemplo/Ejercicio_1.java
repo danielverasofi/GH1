60b7142c4a9d328460dbcaa822ae0ee3ed2cb26c
Laetian <laetianv@gmail.com>
2023-03-23 14:29:55 +0100

    ej 1

---
+++ Test/src/ejercicio_Ejemplo/Ejercicio_1.java
@@ -0,0 +1,18 @@
+package ejercicio_Ejemplo;
+
+public class Ejercicio_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String nombre ="Ana";
+		int edad=27;
+		float salario = 1800.50f;
+		boolean carnet = true;
+		
+		System.out.println(nombre+"\n"+edad+"\n"+salario+"\n"+carnet);
+
+	}
+
+}
---
+++ Test/src/instanceOf/InstanceOf.java
@@ -0,0 +1,15 @@
+package instanceOf;
+
+public class InstanceOf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String texto = "";
+//				System.out.println(texto instanceof);
+		
+
+	}
+
+}
