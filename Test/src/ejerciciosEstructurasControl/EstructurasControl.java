37b0701b3948a031c8923bb32059713b992f694a
Laetian <laetianv@gmail.com>
2023-03-27 08:35:12 +0200

    Update EstructurasControl.java

--- Test/src/ejerciciosEstructurasControl/EstructurasControl.java
+++ Test/src/ejerciciosEstructurasControl/EstructurasControl.java
@@ -8,30 +8,60 @@ package ejerciciosEstructurasControl;
  *
  */
 
-
 public class EstructurasControl {
 
 	/**
 	 * @param args
 	 */
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 
 		ifSimple();
+
+		ifDouble();
 		
-		
+		ifAnidado();
+
 	}
 	
-	//Método de uso del if...
-	
-	public static void ifSimple() {
-		
-		int nota=6;
-		if (nota>=5) {
-			System.out.println("Aprobado!");
+	static int nota=4;
+
+	// Método de uso del if...
+	// El if evalua una condición boleana, si la cumple ejecuta la acción del
+	// interior del parentesis
+	// en caso contrario no ejecuta nada y continua bajando por el código
+	static void ifSimple() {
+		if (nota >= 5) {
+			System.out.println("Aprobado!(ifSimple)");
 		}
 	}
 
+	//En el método anterior si no entraba en la condición se salía de la función,
+	//si añadimos un else recogemos cuando no entra en la condición y ejecutamos el else
+	static void ifDouble() {
+		if (nota >= 5) {
+			System.out.println("Aprobado!(ifDouble)");
+		} else {
+			System.out.println("Suspenso!(ifDouble)");
+		}
+	}
 
+	
+	//Podemos ejecutar varios if, poniendo if else, recogiendo entonces
+	//multiples posibilidades y realizando opciones diferentes para cada uno.
+	static void ifAnidado() {
+		if (nota == 5) {
+System.out.println("Suficiente!(ifAnidado)");
+		} else if(nota==6){
+			System.out.println("Bien!(ifAnidado)");
+		} else if(nota==7 ||nota==8){
+			System.out.println("Notable(ifAnidado)");
+		} else if(nota==9) {
+			System.out.println("Sobresaliente(ifAnidado)");
+		} else if(nota==10) {
+			System.out.println("Matrícula!(ifAnidado)(ifAnidado)");
+		} else {
+			System.out.println("Suspenso!(ifAnidado)");
+		}
+	}
 }
